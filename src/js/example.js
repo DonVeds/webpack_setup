832fb4e17dc25096ac0c0e6b6ac834ff902945a7
Dmitry <donveds@gmail.com>
2019-07-15 18:37:23 +0300

    vuejs branch

---
+++ src/js/example.js
@@ -0,0 +1,10 @@
+// Number.parseFloat(crypto.id).toFixed(2);
+// console.log( if (Number.parseFloat("12270.1883202796878729").toFixed(0) == 0) {Number.parseFloat("12270.1883202796878729").toFixed(6)} });
+
+// const ws = new WebSocket("wws://ws.coincap.io/prices?assets=ALL");
+
+// ws.onopen = () => console.log('ONLINE')
+
+// ws.onclose = () => console.log('DISCONECT')
+
+// ws.onmessage = response => console.log(response)
